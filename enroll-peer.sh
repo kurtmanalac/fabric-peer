a5da90f16fc4ce6d61e08f7cf1b8758f76a0d10e
Kurt <jfmanalac@up.edu.ph>
2025-07-24 01:40:31 +0800

    zip folder

--- enroll-peer.sh
+++ enroll-peer.sh
@@ -12,7 +12,7 @@ command_json=$(jq -n --arg cmd "$command" '{command: $cmd}')
 source=${source:-$CA_URL$FABRIC_CA_CLIENT_HOME/$ENROLL_ID}
 zip_json=$(jq -n --arg src "$source" '{sourcePath: $src}')
 destination=${destination:-$MSP_DIR}
-path_json=$(jq -n --arg src "$source" --arg dest "$destination" '{sourcePath: $src.zip, destinationPath: $dest}')
+path_json=$(jq -n --arg src "$source" --arg dest "$destination" '{sourcePath: ($src + ".zip"), destinationPath: $dest}')
 
 # TLS_CERT_PATH=${TLS_CERT_PATH:-$FABRIC_CA_CLIENT_HOME/ca-cert.pem}
 
