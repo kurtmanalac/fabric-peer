9af4ef369fa843afefcf622f8d5d696b220edaa5
Kurt <jfmanalac@up.edu.ph>
2025-07-23 20:39:50 +0800

    updated script

--- enroll-peer.sh
+++ enroll-peer.sh
@@ -8,11 +8,11 @@ CA_URL=${CA_URL:-http://github-fabric-ca.railway.internal:8000}
 MSP_DIR=${MSP_DIR:-/app/data/msp}
 FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-client}
 command=${command:-fabric-ca-client enroll -u http://$ENROLL_ID:$ENROLL_PW@localhost:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID}
-command_json=$(jq -n --arg cmd $command '{command: $cmd}')
+command_json=$(jq -n --arg cmd "$command" '{command: $cmd}')
 source=${source:-$FABRIC_CA_CLIENT_HOME/$ENROLL_ID}
-source_json=$(jq -n --arg cmd $source '{sourcePath: $cmd}')
+source_json=$(jq -n --arg cmd "$source "'{sourcePath: $cmd}')
 dest=${dest:-$CORE_PEER_MSPCONFIGPATH}
-dest_json=$(jq -n --arg cmd $dest '{destinationPath: $cmd}')
+dest_json=$(jq -n --arg cmd "$dest" '{destinationPath: $cmd}')
 # TLS_CERT_PATH=${TLS_CERT_PATH:-$FABRIC_CA_CLIENT_HOME/ca-cert.pem}
 
 # --- Wait for the CA to be reachable ---
