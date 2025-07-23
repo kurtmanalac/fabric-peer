ff94bd024acd0682586f52581468f0242dd7e476
Kurt <jfmanalac@up.edu.ph>
2025-07-23 22:52:09 +0800

    updated script variables

--- enroll-peer.sh
+++ enroll-peer.sh
@@ -10,7 +10,7 @@ FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-client}
 command=${command:-fabric-ca-client enroll -u http://$ENROLL_ID:$ENROLL_PW@localhost:7054 --mspdir $FABRIC_CA_CLIENT_HOME/$ENROLL_ID}
 command_json=$(jq -n --arg cmd "$command" '{command: $cmd}')
 source=${source:-$FABRIC_CA_CLIENT_HOME/$ENROLL_ID}
-dest=${dest:-$CORE_PEER_MSPCONFIGPATH}
+destination=${dest:-$CORE_PEER_MSPCONFIGPATH}
 path_json=$(jq -n --arg src "$source" --arg dest "$destination" '{sourcePath: $src, destinationPath: $dest}')
 
 # TLS_CERT_PATH=${TLS_CERT_PATH:-$FABRIC_CA_CLIENT_HOME/ca-cert.pem}
