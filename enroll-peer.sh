e357b881a30f100918321dabacd916d639b5f0d0
Kurt <jfmanalac@up.edu.ph>
2025-07-24 02:35:23 +0800

    removed -L flag and adjusted filepaths

--- enroll-peer.sh
+++ enroll-peer.sh
@@ -5,11 +5,11 @@ set -e
 
 # --- CONFIGURATION ---
 CA_URL=${CA_URL:-http://github-fabric-ca.railway.internal:8000}
-MSP_DIR=${MSP_DIR:-/app/data/$ENROLL_ID}
-FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-client}
+MSP_DIR=${MSP_DIR:-/app/data/$ENROLL_ID/}
+FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-client/}
 command=${command:-fabric-ca-client enroll -u http://$ENROLL_ID:$ENROLL_PW@localhost:7054 --mspdir $MSP_DIR}
 command_json=$(jq -n --arg cmd "$command" '{command: $cmd}')
-source=${source:-$CA_URL$FABRIC_CA_CLIENT_HOME/$ENROLL_ID}
+source=${source:-$CA_URL$FABRIC_CA_CLIENT_HOME/$ENROLL_ID/}
 zip_json=$(jq -n --arg src "$source" '{sourcePath: $src, zipPath: ($src+".zip")}')
 destination=${destination:-$MSP_DIR}
 path_json=$(jq -n --arg src "$source" --arg dest "$destination" '{sourcePath: ($src + ".zip"), destinationPath: $dest}')
@@ -29,19 +29,19 @@ curl -X POST $CA_URL/enroll \
 # --- Sync folders to be exposed ---
 echo "Exposing $source..."
 curl -X GET $CA_URL/mkdir/$ENROLL_ID
-curl -L -I "${CA_URL}${FABRIC_CA_CLIENT_HOME}/${ENROLL_ID}"
+curl -I "${CA_URL}${FABRIC_CA_CLIENT_HOME}/${ENROLL_ID}/"
 # --- Copy MSP files ---
 
 echo "Zipping MSP files from $source..."
-curl -L -X POST $CA_URL/zip-folder \
+curl -X POST $CA_URL/zip-folder \
     -H "Content-Type: application/json" \
     -d "$zip_json" &
 ZIP_ID=$!
 wait $ZIP_ID
 
-curl -L -I "${CA_URL}${FABRIC_CA_CLIENT_HOME}/${ENROLL_ID}"
+curl -I "${CA_URL}${FABRIC_CA_CLIENT_HOME}/${ENROLL_ID}/"
 echo "Copying MSP files from $source to $destination..."
-curl -L -X POST $CA_URL/copy-msp \
+curl -X POST $CA_URL/copy-msp \
     -H "Content-Type: application/json" \
     -d "$path_json" &
 COPY_ID=$!
