88a69d81a8eb098b02f1485146da342f58ff07a3
Kurt <jfmanalac@up.edu.ph>
2025-12-09 12:40:26 +0800

    set idType to type

--- enroll-peer.sh
+++ enroll-peer.sh
@@ -9,7 +9,7 @@ set -e
 CA_URL=${CA_URL:-http://github-fabric-ca.railway.internal:8000}
 FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME:-/app/data/fabric-ca-client}
 customCmd=${customCmd:---csr.hosts $RAILWAY_SERVICE_NAME,$RAILWAY_PRIVATE_DOMAIN --csr.names C=US,ST=California,L=SanFrancisco,O=upmo,OU=peer}
-enroll_json=$(jq -n --arg id "$ENROLL_ID" --arg pw "$ENROLL_PW" --arg id "peer" --arg cmd "$customCmd" '{userId: $id, userPw: $pw, idType: $id, customCmd: $cmd}')
+enroll_json=$(jq -n --arg id "$ENROLL_ID" --arg pw "$ENROLL_PW" --arg type "peer" --arg cmd "$customCmd" '{userId: $id, userPw: $pw, idType: $type, customCmd: $cmd}')
 zip_json=$(jq -n --arg src "$FABRIC_CA_CLIENT_HOME/$ENROLL_ID" '{sourceFolder: $src, zipPath: ($src+".zip")}')
 clean_json=$(jq -n --arg script "clean-zip.sh" --arg folder "$FABRIC_CA_CLIENT_HOME/$ENROLL_ID.zip" '{"shellScript": $script, "envVar": {"CLEAN_ID_ZIP": $folder}}')
 
